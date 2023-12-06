7ac707de9e01a593a939349e86664d570c92d93e
adriroca20 <hardhe21@gmail.com>
2023-12-06 22:34:48 +0100

    Added styles and children components

--- projects/00-hola-mundo/src/App.jsx
+++ projects/00-hola-mundo/src/App.jsx
@@ -1,8 +1,18 @@
 import { TwitterFollowCard } from "./TwitterFollowCard"
-import "./index.css"
+import "./app.css"
 
 export const App = ()=>{
     return (
-        <TwitterFollowCard></TwitterFollowCard>
+        <section>
+            <TwitterFollowCard tag={"@adrian_rocafull"} isFollowing={false}>
+                <h1>Adrian</h1>
+            </TwitterFollowCard>
+            <TwitterFollowCard name={"Adrian2"} tag={"@adrian2_rocafull"} isFollowing={true}>
+                <h1>Adrian</h1>
+            </TwitterFollowCard>
+            <TwitterFollowCard name={"Adrian3"} tag={"@adrian3_rocafull"} isFollowing={false}>
+                <h1>Adrian</h1>
+            </TwitterFollowCard>
+        </section>
     )
 }
